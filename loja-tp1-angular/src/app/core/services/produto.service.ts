d00a9ca03768ce7927f7e7b14aa07be27b2289e1
britoGui07 <andradebritoguilherme@gmail.com>
2026-09-10 17:30:50 -0300

    Fix: Alterações e correções estruturais do cherry-pick

---
+++ loja-tp1-angular/src/app/core/services/produto.service.ts
@@ -0,0 +1,57 @@
+import { inject, Injectable } from '@angular/core';
+import { LoggerService } from '../../core/logger/logger.service';
+import { Produto } from '../../model/produto';
+import { delay, Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProdutoService {
+
+  private logger = inject(LoggerService);
+
+  private readonly listaMock = <Produto[]>[
+    {
+      id: 1,
+      nome: 'Mounjaro',
+      preco: 1699.99,
+      descricao: 'Canetas caras demais. Deus me livre.',
+      imageUrl: 'images/mounjaro.jpg',
+      promo: false,
+      estado: 'novo',
+    },
+    {
+      id: 2,
+      nome: 'Ozempic',
+      preco: 1299.94,
+      descricao: 'Continuam caras. Deus continue me livrando.',
+      imageUrl: 'images/ozempic.jpg',
+      promo: false,
+      estado: 'usado',
+    },
+    {
+      id: 3,
+      nome: 'Wegovy',
+      preco: 2500.00,
+      descricao: 'Misericórdia. Deus foi para floripa?',
+      imageUrl: 'images/wegovy.jpg',
+      promo: true,
+      estado: 'esgotado',
+    },
+    {
+      id: 4,
+      nome: 'Novalgina',
+      preco: 17.90,
+      descricao: 'Será que bois sentem dor de cabeça?',
+      imageUrl: 'images/novalgina.jpg',
+      promo: false,
+      estado: 'novo',
+    },
+];
+
+  listar(): Observable<Produto[]>{
+    this.logger.info("PRODUTO SERVICE - retornando lista de produto");
+    return of(this.listaMock).pipe(delay(250))
+
+  }
+}
--- loja-tp1-angular/src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ loja-tp1-angular/src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,6 +1,8 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { Produto } from '../../../model/produto';
-import { CardProduto } from '../card-produto/card-produto';
+import { CardProduto } from "../card-produto/card-produto";
+import { ProdutoService } from '../../../core/services/produto.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -10,59 +12,25 @@ import { CardProduto } from '../card-produto/card-produto';
 })
 export class ListaProdutos {
 
-  apenasPromo = signal(false)
+  private produtoService = inject(ProdutoService);
 
-  produtosExibidos = computed(() => this.apenasPromo() ? this.produtos.filter(p => p.promo) : this.produtos)
+  private produtos = toSignal<Produto[], Produto[]>( this.produtoService.listar(), {initialValue: []});
+  apenasPromo = signal(false);
+
+  produtosExibidos = computed(() =>
+   this.apenasPromo() ? this.produtos().filter(p => p.promo): this.produtos());
 
   alterarPromo(){
     this.apenasPromo.update(v => !v)
   }
 
-  produtos = <Produto[]>[
-    {
-      id: 1,
-      nome: 'Mounjaro',
-      preco: 1699.99,
-      descricao: 'Canetas caras demais. Deus me livre.',
-      imageUrl: 'images/mounjaro.jpg',
-      promo: false,
-      estado: 'novo',
-    },
-    {
-      id: 2,
-      nome: 'Ozempic',
-      preco: 1299.94,
-      descricao: 'Continuam caras. Deus continue me livrando.',
-      imageUrl: 'images/ozempic.jpg',
-      promo: false,
-      estado: 'usado',
-    },
-    {
-      id: 3,
-      nome: 'Wegovy',
-      preco: 2500.00,
-      descricao: 'Misericórdia. Deus foi para floripa?',
-      imageUrl: 'images/wegovy.jpg',
-      promo: true,
-      estado: 'esgotado',
-    },
-    {
-      id: 4,
-      nome: 'Novalgina',
-      preco: 17.90,
-      descricao: 'Será que bois sentem dor de cabeça?',
-      imageUrl: 'images/novalgina.jpg',
-      promo: false,
-      estado: 'novo',
-    },
-    
-  ]
 
-  onViewProduct(id: number){
-    alert(`Visualizando produto id ${id}`)
-  }
-  onAddProduct(produto: {id: number, qtd: number}){
-    alert('Adicionado produto '+produto.id+' | quantidade: '+produto.qtd)
-  }
 
+onViewProduct(id: number){
+  alert('Visualizando produto id: ' +id);
+}
+
+onAddProduct(produto: {id: number, qtd: number}){
+  alert(' Adicionando produto: '+produto.id+' | quantidade: '+produto.qtd);
+}
 }
